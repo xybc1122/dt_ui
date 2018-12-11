082544c1a271ae3e898c9c1746b0d9f36e57e264
陈恩惠 <451597529@qq.com>
2018-12-11 17:26:55 +0800

    2018/12/11

--- src/views/Upload/UploadFile.vue
+++ src/views/Upload/UploadFile.vue
@@ -1,22 +1,37 @@
 <template>
-  <el-upload
-    class="upload-demo"
-    :action="url"
-    drag
-    :on-progress="onProgressFile"
-    :on-preview="handlePreview"
-    :on-remove="handleRemove"
-    :on-success="uploadSuccess"
-    :onError="uploadError"
-    :before-remove="beforeRemove"
-    :before-upload="beforeAvatarUpload"
-    multiple
-    :limit="5"
-    :on-exceed="handleExceed"
-    :file-list="fileList">
-    <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-    <div class="el-upload__tip" slot="tip">只能上传.csv/.xls/.xlsx格式文件/不能超过100MB</div>
-  </el-upload>
+  <div>
+    <div style="margin-top: 20px">
+      <el-radio-group v-model="radio" size="mini" @change="changeRadio">
+        <el-radio-button :label="rArr" v-for="(rArr,index) in radioArr" :key="index">{{rArr.name}}</el-radio-button>
+      </el-radio-group>
+    </div>
+    <el-select v-model="values" placeholder="请选择" @change="changeSelect" value="">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item">
+      </el-option>
+    </el-select>
+    <el-upload
+      class="upload-demo"
+      :action="url"
+      drag
+      :on-progress="onProgressFile"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :on-success="uploadSuccess"
+      :onError="uploadError"
+      :before-remove="beforeRemove"
+      :before-upload="beforeAvatarUpload"
+      multiple
+      :limit="5"
+      :on-exceed="handleExceed"
+      :file-list="fileList" v-if="isFileUp">
+      <div class="el-upload__text">将{{name}}/{{zd}}文件拖到此处，或<em>点击上传</em></div>
+      <div class="el-upload__tip" slot="tip">只能上传.csv/.xls/.xlsx格式文件/不能超过100MB</div>
+    </el-upload>
+  </div>
 </template>
 
 <script>
@@ -26,11 +41,46 @@
   export default {
     data () {
       return {
+        name: '',
+        zd:'',
+        isFileUp: false,
+        values: '',
         fileList: [],
-        url: BASE_URL + '/upload/germany'
+        url: BASE_URL + '/upload/germany',
+        radio: '1',
+        radioArr: [{'id': 1, 'name': '电兔'}, {'id': 2, 'name': 'xx'}, {'id': 3, 'name': 'xxx'}],
+        options: [{
+          value: '1',
+          label: '站点1'
+        }, {
+          value: '2',
+          label: '站点2'
+        }, {
+          value: '3',
+          label: '站点3'
+        }, {
+          value: '4',
+          label: '站点4'
+        }, {
+          value: '5',
+          label: '站点5'
+        }],
       }
     },
     methods: {
+      changeRadio (value) {
+        this.name = value.name
+
+        this.values = ''
+        this.isFileUp = false
+      },
+      changeSelect (value) {
+        console.log(value)
+        if (value) {
+          this.zd=value.label
+          this.isFileUp = true
+        }
+      },
       onProgressFile (event, file, fileList) {
         console.log(event, file, fileList)
       },
@@ -68,15 +118,20 @@
           message.errorMessage('文件不能超过100MB')
           return false
         }
-        // this.fileList.push()
+        return confirm('确定要上传吗？')
+
       },
       //上传成功
       uploadSuccess (success) {
-        console.log(success)
+        if (success.code === -1) {
+          message.errorMessage('上传成功~' + success.msg)
+        } else {
+          message.successMessage(success.msg)
+        }
       },
       // 上传错误
       uploadError (response, file, fileList) {
-        console.log('上传失败，请重试！')
+        message.errorMessage(response.message)
       },
     }
   }
