65e8981a0149e3f2ac2206425ba358687489758b
陈恩惠 <451597529@qq.com>
2018-12-12 16:21:20 +0800

    2018/12/12

--- src/api/index.js
+++ src/api/index.js
@@ -24,8 +24,8 @@ export const repGetHead = ({menuIds}) => ajax(BASE_URL + '/getByHead', {menuIds}
 
 //通过一个mid查询table头信息
 export const repShowByHead = (mId) => ajax(BASE_URL + `/showByHead`, {mId})
-  //获取所有表头的信息
-export const repFindByHeads=()=> ajax(BASE_URL + '/findHeads')
+//获取所有表头的信息
+export const repFindByHeads = () => ajax(BASE_URL + '/findHeads')
 //访问index页面
 export const repIndex = () => ajax(BASE_URL + `/menu/index`)
 
@@ -83,6 +83,9 @@ export const repGetRegionInfo = ({currentPage, pageSize}) => ajax(BASE_URL + `/r
 //获取站点的所有信息
 export const repGetSiteInfo = () => ajax(BASE_URL + `/site/findByListSite`)
 
+//通过shop id 获取站点信息
+export const repGetShopIdSiteInfo = (sId) => ajax(BASE_URL + '/site/getByShopIdListSite', {sId})
+
 //获取币别的所有信息
 export const repGetCurrencyInfo = () => ajax(BASE_URL + `/currency/findByListCurrency`)
 
@@ -111,8 +114,8 @@ export const repDelRole = ({rolesId, uid}) => ajax(BASE_URL + '/ur/delRole', {ro
 export const repAdRole = ({rolesId, uid}) => ajax(BASE_URL + '/ur/addRole', {rolesId, uid}, 'POST')
 
 //新增菜单关联表头字段
-export const repAddHeadMenu=({mId,thIds})=> ajax(BASE_URL + '/hm/saveHeadMenu', {mId, thIds}, 'POST')
+export const repAddHeadMenu = ({mId, thIds}) => ajax(BASE_URL + '/hm/saveHeadMenu', {mId, thIds}, 'POST')
 
 //删除菜单关联表头字段
-export const repDelHeadMenu=({mId,thIds})=> ajax(BASE_URL + '/hm/delTbHeadMenu', {mId, thIds}, 'POST')
+export const repDelHeadMenu = ({mId, thIds}) => ajax(BASE_URL + '/hm/delTbHeadMenu', {mId, thIds}, 'POST')
 
--- src/views/Upload/UploadFile.vue
+++ src/views/Upload/UploadFile.vue
@@ -2,15 +2,15 @@
   <div>
     <div style="margin-top: 20px">
       <el-radio-group v-model="radio" size="mini" @change="changeRadio">
-        <el-radio-button :label="rArr" v-for="(rArr,index) in radioArr" :key="index">{{rArr.name}}</el-radio-button>
+        <el-radio-button :label="sArr" v-for="(sArr,index) in shopArr" :key="index">{{sArr.shopName}}</el-radio-button>
       </el-radio-group>
     </div>
-    <el-select v-model="values" placeholder="请选择" @change="changeSelect" value="">
+    <el-select v-model="uploadFrom.seId" placeholder="请选择" @change="changeSelect" value="">
       <el-option
-        v-for="item in options"
-        :key="item.value"
-        :label="item.label"
-        :value="item">
+        v-for="item in siteOptions"
+        :key="item.siteId"
+        :label="item.siteName"
+        :value="item.siteId">
       </el-option>
     </el-select>
     <el-upload
@@ -24,11 +24,12 @@
       :onError="uploadError"
       :before-remove="beforeRemove"
       :before-upload="beforeAvatarUpload"
+      :data="uploadFrom"
       multiple
       :limit="5"
       :on-exceed="handleExceed"
       :file-list="fileList" v-if="isFileUp">
-      <div class="el-upload__text">将{{name}}/{{zd}}文件拖到此处，或<em>点击上传</em></div>
+      <div class="el-upload__text">将{{shopName}}店铺---{{siteName}}站点---文件拖到此处，或<em>点击上传</em></div>
       <div class="el-upload__tip" slot="tip">只能上传.csv/.xls/.xlsx格式文件/不能超过100MB</div>
     </el-upload>
   </div>
@@ -36,50 +37,56 @@
 
 <script>
   import message from '../../utils/Message'
+  import {
+    repGetShopInfo,
+    repGetShopIdSiteInfo
+  } from '../../api'
 
   const BASE_URL = '/api'
   export default {
     data () {
       return {
-        name: '',
-        zd:'',
+        uploadFrom: {
+          sId: '',//店铺ID
+          seId: ''//站点 ID
+        },
+        shopName: '',//店铺名称
+        siteName: '',//站点名称
         isFileUp: false,
-        values: '',
         fileList: [],
-        url: BASE_URL + '/upload/germany',
-        radio: '1',
-        radioArr: [{'id': 1, 'name': '电兔'}, {'id': 2, 'name': 'xx'}, {'id': 3, 'name': 'xxx'}],
-        options: [{
-          value: '1',
-          label: '站点1'
-        }, {
-          value: '2',
-          label: '站点2'
-        }, {
-          value: '3',
-          label: '站点3'
-        }, {
-          value: '4',
-          label: '站点4'
-        }, {
-          value: '5',
-          label: '站点5'
-        }],
+        url: BASE_URL + '/upload/file',
+        radio: '',
+        shopArr: [],
+        siteOptions: [],//站点信息
+      }
+    },
+    async mounted () {
+      //获取店铺信息
+      const resultShop = await repGetShopInfo()
+      if (resultShop.code === 200) {
+        this.shopArr = resultShop.data
       }
     },
     methods: {
-      changeRadio (value) {
-        this.name = value.name
-
-        this.values = ''
+      async changeRadio (value) {
+        this.shopName = value.shopName
+        this.uploadFrom.sId = value.shopId
+        //通过 sId  获得站点信息
+        const resultSite = await repGetShopIdSiteInfo(this.uploadFrom.sId)
+        if (resultSite.code === 200) {
+          this.siteOptions = resultSite.data
+        }
+        this.uploadFrom.seId = ''
         this.isFileUp = false
       },
+      //下拉时获取 通过value=siteId  查询对应的对象 获取 label
       changeSelect (value) {
-        console.log(value)
-        if (value) {
-          this.zd=value.label
-          this.isFileUp = true
-        }
+        let obj = {}
+        obj = this.siteOptions.find((item) => {
+          return item.siteId === value
+        })
+        this.siteName = obj.siteName
+        this.isFileUp = true
       },
       onProgressFile (event, file, fileList) {
         console.log(event, file, fileList)
