4ee5151cd1dca527379d99bb6706a0655af7db9b
陈恩惠 <451597529@qq.com>
2019-01-04 17:38:00 +0800

    2

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
       '/api': { // 匹配所有以 '/api'开头的请求路径
 
 
-        target: 'http://127.0.0.1:9002', // 代理目标的基础路径
+        target: 'http://127.0.0.1:9001', // 代理目标的基础路径
 
 
         changeOrigin: true, // 支持跨域
